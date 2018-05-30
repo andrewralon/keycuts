2c7d074a7e111eb07aeb2835d3ec3192514d251a
andrewralon <andrew@teamralon.com>
2018-05-30 17:06:00 -0400

    Labels, buttons, margins, oh my

--- keycuts.GUI/MainWindow.xaml.cs
+++ keycuts.GUI/MainWindow.xaml.cs
@@ -24,43 +24,17 @@ namespace keycuts.GUI
     {
         #region Fields
 
-        public string _step1 = "1. Drag in or paste a file, folder, or URL";
-        public string _step2 = "2. Choose a name for the shortcut";
-        public string _step3 = "3. Create the shortcut";
-
         #endregion Fields
 
         #region Properties
 
-        public string Step1
-        {
-            get { return _step1; }
-            set
-            {
-                _step1 = value;
-                OnPropertyChanged();
-            }
-        }
+        public string Step1 { get { return Steps[0]; } }
 
-        public string Step2
-        {
-            get { return _step2; }
-            set
-            {
-                _step2 = value;
-                OnPropertyChanged();
-            }
-        }
+        public string Step2 { get { return Steps[1]; } }
 
-        public string Step3
-        {
-            get { return _step3; }
-            set
-            {
-                _step3 = value;
-                OnPropertyChanged();
-            }
-        }
+        public string Step3 { get { return Steps[2]; } }
+
+        public string Instructions { get { return string.Join("\n", Steps[3], Steps[4], Steps[5]); } }
 
         #endregion Properties
 
@@ -69,9 +43,8 @@ namespace keycuts.GUI
             "1. Drag in or paste a file, folder, or URL",
             "2. Choose a name for the shortcut",
             "3. Create the shortcut",
-            "Instructions:",
-            "* Press Windows + R",
-            "* Type the shortcut name,",
+            "* Type Windows + R",
+            "* Type the shortcut name",
             "* Press enter. Enjoy!"
         };
 

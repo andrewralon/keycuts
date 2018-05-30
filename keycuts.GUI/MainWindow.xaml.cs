25af69910f1b811f9118445a4932a6b417d43ba1
andrewralon <andrew@teamralon.com>
2018-05-30 16:47:07 -0400

    Gooey....

--- keycuts.GUI/MainWindow.xaml.cs
+++ keycuts.GUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,75 @@ namespace keycuts.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+
+        public string _step1 = "1. Drag in or paste a file, folder, or URL";
+        public string _step2 = "2. Choose a name for the shortcut";
+        public string _step3 = "3. Create the shortcut";
+
+        #endregion Fields
+
+        #region Properties
+
+        public string Step1
+        {
+            get { return _step1; }
+            set
+            {
+                _step1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Step2
+        {
+            get { return _step2; }
+            set
+            {
+                _step2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Step3
+        {
+            get { return _step3; }
+            set
+            {
+                _step3 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion Properties
+
+        public string[] Steps = new string[]
+        {
+            "1. Drag in or paste a file, folder, or URL",
+            "2. Choose a name for the shortcut",
+            "3. Create the shortcut",
+            "Instructions:",
+            "* Press Windows + R",
+            "* Type the shortcut name,",
+            "* Press enter. Enjoy!"
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
         }
     }
 }

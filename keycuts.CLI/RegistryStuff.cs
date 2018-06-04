512850a970ae07a6e8f04498a5ccdb3d3cd93802
andrewralon <andrew@teamralon.com>
2018-06-03 21:14:10 -0400

    Closes #2 - Can right click in explorer without selection and get current path

--- keycuts.CLI/RegistryStuff.cs
+++ keycuts.CLI/RegistryStuff.cs
@@ -17,14 +17,6 @@ namespace keycuts.CLI
 
         public static readonly string OutputFolderKeyName = "OutputFolder";
 
-        private static readonly string ContextMenuFile = $@"*\shell\{Program.AppName}";
-
-        private static readonly string ContextMenuFileCommand = $@"*\shell\{Program.AppName}\command";
-
-        private static readonly string ContextMenuFolder = $@"Folder\shell\{Program.AppName}";
-
-        private static readonly string ContextMenuFolderCommand = $@"Folder\shell\{Program.AppName}\command";
-
         public static string ShortcutsFolderPath { get { return $"{OutputFolderStartPath}{Program.AppName}"; } }
 
         public static RegistryKey CreateSubKey(RegistryKey context, string name, bool writable = false)
@@ -69,24 +61,29 @@ namespace keycuts.CLI
 
         public static void CreateRightClickContextMenu()
         {
-            var menu = "Create a keycut to here!";
             var appName = Process.GetCurrentProcess().MainModule.FileName;
-            var command = $"\"{appName}\" \"%1\"";
             var context = Registry.ClassesRoot;
 
-            // File
-            var menuKey = CreateSubKey(context, ContextMenuFile, true);
+            var menu = "Create a keycut to here!";
+            var commandShell = $"\"{appName}\" \"%1\"";
+            var commandBackgroundShell = $"\"{appName}\" \"%V\"";
+
+            var keyDirectoryShell = $@"Directory\shell\{Program.AppName}";
+            var keyDirectoryBackgroundShell = $@"Directory\Background\shell\{Program.AppName}";
+
+            // Directory\shell -- get path of selected file/folder with %1
+            var menuKey = CreateSubKey(context, keyDirectoryShell, true);
             menuKey?.SetValue("", menu);
 
-            var commandKey = CreateSubKey(context, ContextMenuFileCommand, true);
-            commandKey?.SetValue("", command);
+            var commandKey = CreateSubKey(context, $@"{keyDirectoryShell}\command", true);
+            commandKey?.SetValue("", commandShell);
 
-            // Folder
-            menuKey = CreateSubKey(context, ContextMenuFolder, true);
+            // Directory\Background\shell -- get current directory with %V
+            menuKey = CreateSubKey(context, keyDirectoryBackgroundShell, true);
             menuKey?.SetValue("", menu);
 
-            commandKey = CreateSubKey(context, ContextMenuFolderCommand, true);
-            commandKey?.SetValue("", command);
+            commandKey = CreateSubKey(context, $@"{keyDirectoryBackgroundShell}\command", true);
+            commandKey?.SetValue("", commandBackgroundShell);
         }
     }
 }

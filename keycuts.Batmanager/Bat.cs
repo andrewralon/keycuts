8c401bc4f344e8e1dc2a803b868f96e8a276fe97
andrewralon <andrew@teamralon.com>
2018-07-02 20:34:21 -0400

    Revert "#21 Setup of parser.... Need to implement regex; Need to search all files in folder"
    This reverts commit e264f1f2559bbcadcca69ab75bb692957e961cee.

--- keycuts.Batmanager/Bat.cs
+++ keycuts.Batmanager/Bat.cs
@@ -14,10 +14,6 @@ namespace keycuts.Batmanager
         public ShortcutType ShortcutType { get; set; }
         public string OpenWithApp { get; set; }
 
-        public Bat()
-        {
-        }
-
         public Bat(string shortcut, string destination, ShortcutType shortcutType, string openWithApp = "")
         {
             Shortcut = shortcut;

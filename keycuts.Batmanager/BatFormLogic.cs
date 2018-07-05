46e73d20a9e639a5cd2b0714f67e83641ef3da10
andrewralon <andrew@teamralon.com>
2018-07-05 14:54:40 -0400

    #21 I mean why not

--- keycuts.Batmanager/BatFormLogic.cs
+++ keycuts.Batmanager/BatFormLogic.cs
@@ -11,6 +11,28 @@ namespace keycuts.Batmanager
 {
     public class BatFormLogic
     {
+        #region Fields
+
+        private static string alphanumeric = "A-Za-z0-9";
+        private static string alphanumericspecial = $"{alphanumeric}:\\.";
+        private static string alphanumericurl = $@"{alphanumeric}:/.";
+
+        public static string URL = $"START [{alphanumericurl}]+";
+        public static string FILE = $"START \"\" /B \"[{alphanumericspecial}]+";
+        public static string FOLDER = $"\"%SystemRoot%\\explorer.exe\" \"[{alphanumericspecial}]+\"";
+        public static string OPENWITHAPP = $"\"[{alphanumericspecial}]+\" \"[{alphanumericspecial}]+\"";
+
+        public static List<BatParseArg> BatList = new List<BatParseArg>()
+        {
+            new BatParseArg(URL, ShortcutType.Url),
+            new BatParseArg(FILE, ShortcutType.File),
+            new BatParseArg(FOLDER, ShortcutType.Folder)
+        };
+
+        #endregion Fields
+
+        #region Public Methods
+
         public static void PopulateDataGrid(DataGrid dataGrid)
         {
             var bats = new List<Bat>();
@@ -25,7 +47,7 @@ namespace keycuts.Batmanager
                         (x.StartsWith("START", StringComparison.CurrentCultureIgnoreCase) || x.Contains("\\explorer.exe")))
                     .ToList();
 
-                var bat = BatParser.Parse(shortcut, lines);
+                var bat = Parse(shortcut, lines);
 
                 if (bat != null)
                 {
@@ -35,5 +57,39 @@ namespace keycuts.Batmanager
 
             dataGrid.ItemsSource = bats;
         }
+
+        public static Bat Parse(string batFile, List<string> lines)
+        {
+            var bat = new Bat();
+
+            if (lines.Any())
+            {
+                bat.Shortcut = Path.GetFileNameWithoutExtension(batFile);
+                bat.Command = lines[0];
+
+                if (lines[0].Contains("\\explorer.exe"))
+                {
+                    // It's a folder!
+                    bat.ShortcutType = ShortcutType.Folder;
+                }
+                else if (lines[0].Substring(0, 5).ToUpper() == "START")
+                {
+                    // It's NOT a folder! -- Could be File, Url, HostsFile, or CLSIDKey
+
+
+                    bat.ShortcutType = ShortcutType.File;
+                }
+            }
+
+            if (string.IsNullOrEmpty(bat.Command) ||
+                (string.IsNullOrEmpty(bat.Shortcut) && string.IsNullOrEmpty(bat.Destination)))
+            {
+                bat = null;
+            }
+
+            return bat;
+        }
+
+        #endregion Public Methods
     }
 }

1828aad41d2015a33766e2de7ea538466b014a34
andrewralon <andrew@teamralon.com>
2018-08-13 00:11:04 -0400

    Fixes #39 Accepts .exe.bat keycuts now

--- keycuts.Common/Shortcut.cs
+++ keycuts.Common/Shortcut.cs
@@ -42,7 +42,9 @@ namespace keycuts.Common
             Type = GetShortcutType(destination, out string newDestination);
             Destination = newDestination;
             Extension = ".bat";
-            Filename = Path.GetFileNameWithoutExtension(shortcut);
+            Filename = shortcut.EndsWith(".exe", StringComparison.InvariantCultureIgnoreCase) ? 
+                shortcut :
+                Path.GetFileNameWithoutExtension(shortcut);
             FilenameWithExtension = $"{Filename}{Extension}";
             Folder = IsNotFullPath(shortcut) ?
                 defaultFolder :
